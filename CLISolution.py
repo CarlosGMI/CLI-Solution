380240fc73f9f1a3327e5a9f83f8e6bea108962c
CarlosGMI <carlosmaldonado@correo.unimet.edu.ve>
2018-08-02 17:47:40 -0400

    Added setup.py

--- CLISolution.py
+++ CLISolution.py
@@ -4,12 +4,14 @@ import click
 __author__ = "Carlos Maldonado"
 
 @click.command()
-@click.argument('filename')
-@click.option('-n')
-def main(filename, n):
-    click.echo('El filename es: %s' % filename)
-    if n:
-        click.echo('Hola %s' % n)
+@click.argument('path_to_read')
+@click.option('--name')
+@click.option('--path')
+def main(path_to_read, name, path):
+    click.echo('El path a leer es: %s' % path_to_read)
+    if name and path:
+        click.echo('--name = %s' % name)
+        click.echo('--path = %s' % path)
 
 if __name__ == "__main__":
     main()
