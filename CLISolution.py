7c9a6b004239fc8bea1bcfbd2c9b8e19639a13ee
CarlosGMI <carlosmaldonado@correo.unimet.edu.ve>
2018-08-05 16:51:48 -0400

    File created and saved in given path, callback functions created for the options
    Common callback to path and name tobe tested

--- CLISolution.py
+++ CLISolution.py
@@ -13,15 +13,27 @@ def readPath(path):
 
 def copyFilesInTextFile(files,name,path):
     print("Procederé a copiar los archivos en {} en: {}".format(name,path))
+    try:
+        textFile = open(join(path,name),'w')
+        textFile.write("Estoy escribiendo en un txt :D")
+        textFile.close()
+    except:
+        raise click.ClickException("Ha ocurrido algún error durante la creación del archivo")
+        
+def validateNameFile(ctx, param, value):
+    if not value.endswith('.txt'):
+        raise click.BadParameter('El nombre del archivo debe ser .txt')
+    else:
+        return value
+
 
 @click.command()
 @click.argument('path_to_read', type=click.Path(exists=True))
-@click.option('--name', '-n', help='El nombre del archivo txt donde se guardarán los nombres de los archivos analizados',
-                default='result.txt')
-@click.option('--path', '-p', help='La ruta específica donde se guardará el archivo txt', type=click.Path(exists=True),
-                default=path.join(path.join(environ['USERPROFILE']), 'Desktop') )
+@click.option('--name', '-n', help='El nombre del archivo txt donde se guardarán los nombres de los archivos analizados. Por defecto el nombre es "result.txt"',
+                default='result.txt', callback=validateNameFile)
+@click.option('--path', '-p', help='La ruta específica donde se guardará el archivo txt. Por defecto la ruta es en el escritorio', type=click.Path(exists=True, file_okay=False),
+                default=path.join(path.join(environ['USERPROFILE']), 'Desktop'))
 def main(path_to_read, name, path):
-    #click.echo('El path a leer es: %s...' % path_to_read)
     copyFilesInTextFile(readPath(path_to_read),name,path)
 
 
